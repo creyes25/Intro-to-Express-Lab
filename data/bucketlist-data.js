83c7cead5feba2c36d57fc7e8cf506db9186c6b8
creyes25 <carolina.reyes2022@gmail.com>
2022-09-23 15:40:41 -0500

    Fix list to display all of the data

--- data/bucketlist-data.js
+++ data/bucketlist-data.js
@@ -1,9 +1,7 @@
 const bucketList = [
-  {
-    text: 'Find a new hobby', complete: true, _id: 1234,
-    text: 'Go skydiving', complete: false, _id: 3453,
-    text: 'Travel', complete: true, _id: 6789, 
-  }
+  {bucket: 'Find a new hobby', complete: true, _id: 1234},
+  {bucket: 'Go skydiving', complete: false, _id: 3453},
+  {bucket: 'Travel', complete: true, _id: 6789}, 
 ]
 
 export {
